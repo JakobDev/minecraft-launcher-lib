e7de3379006d1cfcf05d371cfc7d751f616e3c90
JakobDev <jakobdev@gmx.de>
2019-11-18 16:10:42 +0000

    Set master_doc

--- doc/conf.py
+++ doc/conf.py
@@ -24,6 +24,7 @@ author = 'JakobDev'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
