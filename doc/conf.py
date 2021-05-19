79484bd55793b9227ca243976ac4b038413f9d26
JakobDev <jakobdev@gmx.de>
2021-05-19 13:42:40 +0200

    Change Version to 3.2

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ copyright = '2019-2021, JakobDev'
 author = 'JakobDev'
 
 # The full version, including alpha/beta/rc tags
-release = "3.1"
+release = "3.2"
 
 master_doc = 'index'
 
--- minecraft_launcher_lib/utils.py
+++ minecraft_launcher_lib/utils.py
@@ -90,7 +90,7 @@ def get_library_version() -> str:
     """
     Returns the version of minecraft-launcher-lib
     """
-    return "3.1"
+    return "3.2"
 
 def generate_test_options() -> Dict[str,str]:
     """
