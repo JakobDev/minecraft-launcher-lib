13105fc99cd62ec1891a111c67caf8af0f1ebd8d
JakobDev <jakobdev@gmx.de>
2021-06-15 13:37:33 +0200

    Better tests for get_minecraft_command()

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,3 +1,4 @@
+from typing import List
 import minecraft_launcher_lib
 import requests
 import pytest
@@ -5,7 +6,19 @@ import shutil
 import os
 
 
-def test_get_minecraft_command(tmpdir):
+def check_argument(command: List[str], argument: str, value: str) -> bool:
+    # Checks if a argument has the given value
+    try:
+        index = command.index(argument)
+    except:
+        return False
+    try:
+        return command[index + 1] == value
+    except:
+        return False
+
+
+def download_test_version(tmpdir) -> str:
     # Download a random version for test
     version_list = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").json()
     version_id = version_list["versions"][0]["id"]
@@ -14,10 +27,93 @@ def test_get_minecraft_command(tmpdir):
     with open(os.path.join(tmpdir, "versions", version_id, f"{version_id}.json"), 'wb') as f:
         r.raw.decode_content = True
         shutil.copyfileobj(r.raw, f)
-    # Get the command
+    return version_id
+
+
+def test_get_minecraft_command_executable_basic(tmpdir):
+    version_id = download_test_version(tmpdir)
+    options = {}
+    options["username"] = "Player"
+    options["uuid"] = "Testuuid"
+    options["token"] = "testtoken"
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert check_argument(command, "--username", "Player")
+    assert check_argument(command, "--uuid", "Testuuid")
+    assert check_argument(command, "--accessToken", "testtoken")
+
+
+def test_get_minecraft_command_executable_path(tmpdir):
+    version_id = download_test_version(tmpdir)
+    # Test with option
+    options = {"executablePath": "test"}
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert command[0] == "test"
+    # Test without option
     command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), {})
-    for i in command:
-        assert isinstance(i, str)
+    assert command[0] == "java"
+
+
+def test_get_minecraft_command_jvm_arguments(tmpdir):
+    version_id = download_test_version(tmpdir)
+    options = {"jvmArguments": ["a", "b"]}
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert command[1] == "a"
+    assert command[2] == "b"
+
+
+def test_get_minecraft_command_game_directory(tmpdir):
+    version_id = download_test_version(tmpdir)
+    # Test with option
+    options = {"gameDirectory": "gamedir"}
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert check_argument(command, "--gameDir", "gamedir")
+    # Test without option
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), {})
+    assert check_argument(command, "--gameDir", str(tmpdir))
+
+
+def test_get_minecraft_command_demo(tmpdir):
+    version_id = download_test_version(tmpdir)
+    # Test with option
+    options = {"demo": True}
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert "--demo" in command
+    # Test without option
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), {})
+    assert "--demo" not in command
+
+
+def test_get_minecraft_command_custom_resolution(tmpdir):
+    version_id = download_test_version(tmpdir)
+    # Test default values of width and height
+    options = {"customResolution": True}
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert check_argument(command, "--width", "854")
+    assert check_argument(command, "--height", "480")
+    # Test custom values of width and height
+    options = {}
+    options["customResolution"] = True
+    options["resolutionWidth"] = "1000"
+    options["resolutionHeight"] = "500"
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert check_argument(command, "--width", "1000")
+    assert check_argument(command, "--height", "500")
+    # Test without options
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), {})
+    assert "--width" not in command
+    assert "--height" not in command
+
+
+def test_get_minecraft_command_natives_directory(tmpdir):
+    version_id = download_test_version(tmpdir)
+    # Test with option
+    options = {"nativesDirectory": "test"}
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), options)
+    assert "-Djava.library.path=test" in command
+    # Test without option
+    command = minecraft_launcher_lib.command.get_minecraft_command(version_id, str(tmpdir), {})
+    default_natives_path = os.path.join(tmpdir, "versions", version_id, "natives")
+    assert f"-Djava.library.path={default_natives_path}"
 
 
 def test_get_minecraft_command_invalid_version(tmpdir):
