9abe5c12d30c3cf4b43e6a0cd665f99934395298
JakobDev <jakobdev@gmx.de>
2021-10-22 18:02:37 +0200

    Add reredirects to new locations in doc

--- doc/conf.py
+++ doc/conf.py
@@ -31,6 +31,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_reredirects"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +54,17 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+redirects = {
+    "account": "modules/account.html",
+    "command": "modules/command.html",
+    "exceptions": "modules/exceptions.html",
+    "fabric": "modules/fabric.html",
+    "forge": "modules/forge.html",
+    "install": "modules/install.html",
+    "microsoft_account": "modules/microsoft_account.html",
+    "natives": "modules/natives.html",
+    "runtime": "modules/runtime.html",
+    "utils": "modules/utils.html",
+}
+
