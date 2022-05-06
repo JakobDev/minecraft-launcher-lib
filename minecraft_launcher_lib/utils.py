00f11233dd62c505d9e3647939cec6f3c273c605
JakobDev <jakobdev@gmx.de>
2022-05-06 09:38:41 +0000

    Use default Path if APPDATA is not set

--- minecraft_launcher_lib/utils.py
+++ minecraft_launcher_lib/utils.py
@@ -15,7 +15,7 @@ def get_minecraft_directory() -> str:
     Returns the default path to the .minecraft directory
     """
     if platform.system() == "Windows":
-        return os.path.join(os.getenv('APPDATA'), ".minecraft")
+        return os.path.join(os.getenv("APPDATA", os.path.join(pathlib.Path.home(), "AppData", "Roaming")), ".minecraft")
     elif platform.system() == "Darwin":
         return os.path.join(str(pathlib.Path.home()), "Library", "Application Support", "minecraft")
     else:
