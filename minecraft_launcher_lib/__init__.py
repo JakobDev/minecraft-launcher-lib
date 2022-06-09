c21fde8ee7b153a353b3d6662b1c0addba8959ec
JakobDev <jakobdev@gmx.de>
2022-06-09 10:45:31 +0000

    Add types module to __all__

--- minecraft_launcher_lib/__init__.py
+++ minecraft_launcher_lib/__init__.py
@@ -1,2 +1,2 @@
 from . import command, install, account, microsoft_account, utils, forge, fabric, runtime, exceptions
-__all__ = ["command", "install", "account", "microsoft_account", "utils", "forge", "fabric", "runtime", "exceptions"]
+__all__ = ["command", "install", "account", "microsoft_account", "utils", "forge", "fabric", "runtime", "exceptions", "types"]
