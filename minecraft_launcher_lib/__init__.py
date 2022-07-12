6c65d1437b39da7f13448a3fabe40484c7050f43
JakobDev <jakobdev@gmx.de>
2022-07-12 14:09:42 +0000

    Add microsoft_types to __all__

--- minecraft_launcher_lib/__init__.py
+++ minecraft_launcher_lib/__init__.py
@@ -1,2 +1,2 @@
-from . import command, install, account, microsoft_account, utils, forge, fabric, runtime, exceptions
-__all__ = ["command", "install", "account", "microsoft_account", "utils", "forge", "fabric", "runtime", "exceptions", "types"]
+from . import command, install, account, microsoft_account, utils, forge, fabric, runtime, exceptions, types, microsoft_types
+__all__ = ["command", "install", "account", "microsoft_account", "utils", "forge", "fabric", "runtime", "exceptions", "types", "microsoft_types"]
