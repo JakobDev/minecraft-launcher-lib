640dab64e5cd268f935fa53f93f82df9b74d67ad
JakobDev <jakobdev@gmx.de>
2023-02-21 15:40:21 +0100

    Remove deprecated account module

--- minecraft_launcher_lib/__init__.py
+++ minecraft_launcher_lib/__init__.py
@@ -1,2 +1,2 @@
-from . import command, install, account, microsoft_account, utils, forge, fabric, runtime, exceptions, types, microsoft_types
-__all__ = ["command", "install", "account", "microsoft_account", "utils", "forge", "fabric", "runtime", "exceptions", "types", "microsoft_types"]
+from . import command, install, microsoft_account, utils, forge, fabric, runtime, exceptions, types, microsoft_types
+__all__ = ["command", "install", "microsoft_account", "utils", "forge", "fabric", "runtime", "exceptions", "types", "microsoft_types"]
